8d8e58ab52cbe6b5e6d7d63967ba9773e4c730d6
Usama Altaf <ualtaf238@gmail.com>
2024-05-29 22:36:37 +0500

    tab is done

--- popup.js
+++ popup.js
@@ -1,18 +1,29 @@
-
-// To get Active tab
 function openTab(evt, tabName) {
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    evt.currentTarget.className += " active";
+ // Declare all variables
+ var i, tabcontent, tablinks;
+
+ tabcontent = document.getElementsByClassName("tabContent");
+ for (i = 0; i < tabcontent.length; i++) {
+   tabcontent[i].style.display = "none";
+ }
+
+ // Get all elements with class="tablinks" and remove the class "active"
+ tablinks = document.getElementsByClassName("tablinks");
+ for (i = 0; i < tablinks.length; i++) {
+   tablinks[i].className = tablinks[i].className.replace(" active", "");
+ }
+
+ // Show the current tab, and add an "active" class to the button that opened the tab
+ document.getElementById(tabName).style.display = "block";
+ evt.currentTarget.className += " active";
 }
 
+
+document.addEventListener('DOMContentLoaded',  function () {
+    document.getElementById("newBookmarkBtn").click();
+})
+
 document.addEventListener('DOMContentLoaded', async function () {
-    var firstTablink = document.getElementsByClassName("tablinks")[0];
-    if (firstTablink) {
-        firstTablink.click();
-    }
     if (chrome.tabs) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (tabs.length > 0) {
